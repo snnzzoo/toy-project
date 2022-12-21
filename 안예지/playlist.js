6884c7a18f52d2edfe890fff683f6d50b676a930
avocad06 <miggo2704@gmail.com>
2022-12-21 15:34:00 +0900

    feat: 로컬에 저장하고 불러오기

--- 안예지/playlist.js
+++ 안예지/playlist.js
@@ -16,9 +16,10 @@ let editID = "";
 form.addEventListener('submit', addItem)
 // 모두 지우기
 clearBtn.addEventListener('click', clearItems);
-
+// load items
+window.addEventListener('DOMContentLoaded', setupItems);
 const deleteBtn = document.querySelector('.delete-btn');
-console.log(deleteBtn);
+// console.log(deleteBtn);
 // 콜백함수 addItem를 따로 작성
 function addItem(e) {
     // form의 기본 이벤트를 제거한다.
@@ -37,35 +38,37 @@ function addItem(e) {
     /* 빈 문자열이 아니고, 편집모드가 아닌 경우
     => 리스트에 아이템을 추가할 때 */
     if (value && !editFlag) {
-        // console.log('add item to the list')
-        const element = document.createElement('article');
-        // add class
-        element.classList.add('playlist-item');
-        // add id
-        const attr = document.createAttribute('data-id');
-        attr.value = id;
-        element.setAttributeNode(attr);
-        element.insertAdjacentHTML('beforeend', `                    
-        <p class="title">${value}</p>
-        <div class="btn-container">
-            <button type="button" class="edit-btn">
-                <span class="material-symbols-outlined">
-                    edit_note
-                </span>
-            </button>
-            <button type="button" class="delete-btn">
-                <span class="material-symbols-outlined">
-                    remove
-                </span>
-            </button>
-        </div>`);
-        const deleteBtn = element.querySelector('.delete-btn')
-        const editBtn = element.querySelector('.edit-btn');
-        deleteBtn.addEventListener('click', deleteItem);
-        editBtn.addEventListener('click', editItem);
+        // // console.log('add item to the list')
+        // const element = document.createElement('article');
+        // // add class
+        // element.classList.add('playlist-item');
+        // // add id
+        // const attr = document.createAttribute('data-id');
+        // attr.value = id;
+        // element.setAttributeNode(attr);
+        // element.insertAdjacentHTML('beforeend', `                    
+        // <p class="title">${value}</p>
+        // <div class="btn-container">
+        //     <button type="button" class="edit-btn">
+        //         <span class="material-symbols-outlined">
+        //             edit_note
+        //         </span>
+        //     </button>
+        //     <button type="button" class="delete-btn">
+        //         <span class="material-symbols-outlined">
+        //             remove
+        //         </span>
+        //     </button>
+        // </div>`);
+        // const deleteBtn = element.querySelector('.delete-btn')
+        // const editBtn = element.querySelector('.edit-btn');
+        // deleteBtn.addEventListener('click', deleteItem);
+        // editBtn.addEventListener('click', editItem);
 
-        // append child
-        list.appendChild(element);
+        // // append child
+        // list.appendChild(element);
+        /* ==========요기까지 createListItem 함수로 대체========= */
+        createListItem(id, value)
         // display alert
         displayAlert('항목이 추가되었습니다.', 'success');
         // show container
@@ -118,7 +121,7 @@ function clearItems() {
     container.classList.remove('show-container')
     displayAlert('목록이 초기화되었습니다.', 'success')
     setBackToDefault();
-    // localStorage.removeItem('list');
+    localStorage.removeItem('list');
 }
 // delete function
 function deleteItem(e) {
@@ -132,7 +135,7 @@ function deleteItem(e) {
     displayAlert('항목이 삭제되었습니다', 'danger');
     setBackToDefault();
     // remove from local storage
-    // removeFromLocalStorage(id);
+    removeFromLocalStorage(id);
 }
 // edit function
 function editItem(e) {
@@ -158,13 +161,92 @@ function setBackToDefault() {
 }
 // *********** LOCAL STORAGE ***********
 function addToLocalStorage(id, value) {
-    console.log('added to local storage');
+    // const playlist = { id: id, value: value };
+    // 매개변수와 키의 이름이 같으면 생략해도 된다.(?)
+    // 아마도 함수의 특징인가?
+    // const playlist = { identify: id, value: value };
+    const playlist = { id, value };
+    // console.log(playlist);
+    let items = getLocalStorage();
+    console.log(items);
+    // console.log('added to local storage');
+    items.push(playlist);
+    localStorage.setItem('list', JSON.stringify(items));
+
 }
-function removeFromLocalStorage(id) { }
-function editLocalStorage(id, value) { }
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+
+    items = items.filter(function (item) {
+        // 뭔 뜻일까? item.id !== id
+        if (item.id !== id) {
+            return item
+        }
+    });
+    localStorage.setItem('list', JSON.stringify(items));
+}
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+    items = items.map(function (item) {
+        if (items.id === id) {
+            item.value = value;
+        }
+        return item;
+    });
+    localStorage.setItem('list', JSON.stringify(items));
+}
+function getLocalStorage() {
+    return localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : [];
+}
 // localStorage API
 // setItem
 // getItem
 // removeItem
 // save as strings
+// localStorage.setItem('오렌지', JSON.stringify(['item', 'item2']));
+// const oranges = JSON.parse(localStorage.getItem('orange'));
+// console.log("oranges");
+// localStorage.removeItem("오렌지");
+
 // *********** SETUP ITEMS ***********
+function setupItems() {
+    let items = getLocalStorage();
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            createListItem(item.id, item.value)
+        })
+        container.classList.add('show-container')
+    }
+}
+
+function createListItem(id, value) {
+    // console.log('add item to the list')
+    const element = document.createElement('article');
+    // add class
+    element.classList.add('playlist-item');
+    // add id
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.insertAdjacentHTML('beforeend', `                    
+     <p class="title">${value}</p>
+     <div class="btn-container">
+         <button type="button" class="edit-btn">
+             <span class="material-symbols-outlined">
+                 edit_note
+             </span>
+         </button>
+         <button type="button" class="delete-btn">
+             <span class="material-symbols-outlined">
+                 remove
+             </span>
+         </button>
+     </div>`);
+    const deleteBtn = element.querySelector('.delete-btn')
+    const editBtn = element.querySelector('.edit-btn');
+    deleteBtn.addEventListener('click', deleteItem);
+    editBtn.addEventListener('click', editItem);
+
+    // append child
+    list.appendChild(element);
+}
