6118e4bee1c13cc81384604dd96c89a3ed46e69e
Sunjoo IM <tjss1580@gmail.com>
2022-12-24 00:49:04 +0900

    Add : toyproject 10

--- 임선주/10-scroll/app.js
+++ 임선주/10-scroll/app.js
@@ -4,10 +4,79 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+// footer 현재 년도로 설정
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+// 반응형 네브바 토글
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+  // 높이 자동 측정 => 리스트 개수가 늘거나 줄 때도 네브바 메뉴에 맞게 높이 자동조절!
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  // console.log(containerHeight);
+  const linksHeight = links.getBoundingClientRect().height;
+  // console.log(linksHeight);
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+window.addEventListener("scroll", function () {
+  // console.log(window.pageYOffset);
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
 
+  // top 버튼
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    // console.log(id);
+    const element = document.getElementById(id);
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+    // console.log(position);
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    // 작은 화면에서
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
