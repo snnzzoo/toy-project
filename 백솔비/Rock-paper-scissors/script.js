5dd90a60e0a4478a1eb170400e3a27375aa5f6ec
hae-on <solbi2004@naver.com>
2022-12-31 01:01:36 +0900

    게임 로직 구현

--- 백솔비/Rock-paper-scissors/script.js
+++ 백솔비/Rock-paper-scissors/script.js
@@ -5,19 +5,8 @@ const paper = document.querySelector('.paper');
 const computerScoreResult = document.querySelector('.computer__score');
 const userScoreResult = document.querySelector('.user__score');
 // 변수
-const computerScore = 0;
-const userScore = 0;
-const rockPaperScissors = ['rock', 'scissors', 'paper'];
-// *3을 하면 0~2 사이의 수 출력, Math.floor로 정수만 출력
-const randomNumber = Math.floor(Math.random() * 3);
-
-function addUserScore(userScore) {
-  console.log(userScore);
-}
-
-function choiceComputer() {
-  return rockPaperScissors[randomNumber];
-}
+let computerScore = 0;
+let userScore = 0;
 
 function win() {
   userScore++;
@@ -31,7 +20,38 @@ function lose() {
   userScoreResult.innerHTML = userScore;
 }
 
+function game(user, computer) {
+  if (user === computer) {
+    console.log('무승부');
+  } else {
+    switch (user + computer) {
+      case '✌🏻✋🏻':
+      case '✊🏻✌🏻':
+      case '✋🏻✊🏻':
+        win();
+        console.log('사용자 승리');
+        break;
+      case '✌🏻✊🏻':
+      case '✊🏻✋🏻':
+      case '✋🏻✌🏻':
+        lose();
+        console.log('컴퓨터 승리');
+        break;
+    }
+  }
+}
+
+function play(event) {
+  const rockPaperScissors = ['✊🏻', '✌🏻', '✋🏻'];
+  // *3을 하면 0~2 사이의 수 출력, Math.floor로 정수만 출력
+  const randomNumber = Math.floor(Math.random() * 3);
+  const computer = rockPaperScissors[randomNumber];
+  const user = event.target.innerText;
+
+  game(user, computer);
+}
+
 // 각 버튼 눌렀을 때 함수 실행
-rock.addEventListener('click', play());
-scissors.addEventListener('click', play());
-paper.addEventListener('click', play());
+rock.addEventListener('click', play);
+scissors.addEventListener('click', play);
+paper.addEventListener('click', play);
