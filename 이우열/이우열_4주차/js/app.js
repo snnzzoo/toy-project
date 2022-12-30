763519adaf1a35fd7afef4cf415306c31e32853b
wooyeol <dnduf158@gmail.com>
2022-12-31 02:24:52 +0900

    이우열-4주차

---
+++ 이우열/이우열_4주차/js/app.js
@@ -0,0 +1,87 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+// offsetTop - A Number, representing the top position of the element, in pixels
+
+// ************** set date **************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+// ************** close links **************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+  // linksContainer.classList.toggle('show-links');
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+// ************** fixed navbar **************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function() {
+  // pageYOffset은 오래된 코드 => scrollY로 바뀜
+  const scrollHeight = window.scrollY;
+  const navHeight = navbar.getBoundingClientRect().height;
+  const logo = this.document.querySelector('.logo');
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+    logo.src = './images/logo_black.svg';
+  } else {
+    navbar.classList.remove('fixed-nav');
+    logo.src = './images/logo.svg';
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
+
+// ************** smooth scroll **************
+
+// select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link) {
+  link.addEventListener('click', function (e) {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    
+    // calculate te heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    
+    let position = element.offsetTop - navHeight;
+    
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    
+    window.scrollTo({
+      left: 0, 
+      top: position,
+    });
+
+    linksContainer.style.height = 0;
+  });
+});
